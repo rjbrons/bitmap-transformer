78a7be7084cf972eb53c6809bddaab1f0c672e6f
Robert Bronson <robert.j.bronson+cf@gmail.com>
2019-05-31 13:20:01 -0700

    finished 3 transforms

--- src/main/java/bitmap/transformer/App.java
+++ src/main/java/bitmap/transformer/App.java
@@ -3,14 +3,39 @@
  */
 package bitmap.transformer;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
 public class App {
     public String getGreeting() {
         return "Hello world.";
     }
 
-    //method to pull in args and create a new instance of Bitmap
+    //pull in args and create a new instance of Bitmap
 
     public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+        String pathIn = args[0];
+        String pathOut = args[1];
+        String xform = args[2];
+        Bitmap bitmap = new Bitmap();
+        BufferedImage inputImage = null;
+
+        try {
+           inputImage =  ImageIO.read(new File(pathIn));
+           if (xform.equals("black") ){
+               inputImage = bitmap.blackout(inputImage);
+           } else if (xform.equals("negative")) {
+               inputImage = bitmap.negativeImage(inputImage);
+            } else if (xform.equals("grey")){
+               inputImage = bitmap.greyScale((inputImage));
+           }
+
+           ImageIO.write(inputImage, "bmp", new File(pathOut));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+//        System.out.println(new App().getGreeting());
     }
 }
